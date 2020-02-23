fd824261592782a472debf175a70667c0a2ec60b
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-02-23 17:01:45 -0300

    adding environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
-  username: 'postgres',
-  password: 'docker',
-  database: 'gympoint',
-  host: '127.0.0.1',
-  dialect: 'postgres',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || '',
+  host: process.env.DB_HOST || '',
+  dialect: process.env.DB_DIALECT || 'postgres',
   define: {
     timestamps: true,
     underscored: true,
