8214d070ab74bb8aa725a0bd691e607c390fa01e
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-02-26 10:53:13 -0300

    student update functionality implemented

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -31,6 +31,39 @@ class StudentController {
 
     return res.json(student);
   }
+
+  async update(req, res) {
+    const schema = yup.object().shape({
+      id: yup
+        .number()
+        .integer()
+        .required(),
+      name: yup.string(),
+      email: yup.string().email(),
+      age: yup
+        .number()
+        .moreThan(0)
+        .integer(),
+      weight: yup.number().moreThan(0),
+      height: yup.number().moreThan(0),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(422).json('Fields validation failed...');
+    }
+
+    const { id } = req.body;
+
+    let student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(404).json('Student not found...');
+    }
+
+    student = await student.update(req.body);
+
+    return res.json(student);
+  }
 }
 
 export default new StudentController();
